dcfd487b992d45cdca7591737caf24418b2cd7f3
anthonyvigz <anthonyvigz@gmail.com>
2020-05-07 15:36:52 -0400

    set up routes, templates, and bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'AnthonyV',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'May 7, 2020'
+    },
+    {
+        'author': 'JaneD',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'May 8, 2020'
+    }
+]
+
 # Create your views here.
+
+def home(request):
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
