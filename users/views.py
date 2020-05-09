7ca8bd324920c942679b31d096dc81970e5d8491
anthonyvigz <anthonyvigz@gmail.com>
2020-05-09 17:13:46 -0400

    set up user form for register

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
@@ -8,8 +8,10 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_date.get('username')
+            form.save()
+            username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
